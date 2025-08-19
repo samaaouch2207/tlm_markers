c0433418ecf2a97223fc89a3ea24ba1061f9dcde
s2operator <s2operator@pros2ipf056052.adwaiseo.local>
2025-08-19 11:35:47 +0000

    Clean branch main

--- launch_all_tlm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-
-./sentinel-2-jp2-tlm/benchmark_table.sh DS2_KAYRROS small 
-./sentinel-2-jp2-tlm/benchmark_table.sh DS2_KAYRROS small2t 
-./sentinel-2-jp2-tlm/benchmark_table.sh DS2_KAYRROS medium 
-./sentinel-2-jp2-tlm/benchmark_table.sh DS2_KAYRROS big 
-./sentinel-2-jp2-tlm/benchmark_table.sh DS2_KAYRROS huge 
